3e3919817b5525c7410fdf9ad642d161a25e6637
Philippe Weier <pweier@adobe.com>
2023-09-18 18:25:57 +0200

    added binary cross entropy loss

--- src/loss.cu
+++ src/loss.cu
@@ -37,6 +37,7 @@
 #include <tiny-cuda-nn/losses/relative_l2.h>
 #include <tiny-cuda-nn/losses/relative_l2_luminance.h>
 #include <tiny-cuda-nn/losses/cross_entropy.h>
+#include <tiny-cuda-nn/losses/binary_cross_entropy.h>
 #include <tiny-cuda-nn/losses/variance_is.h>
 
 namespace tcnn {
@@ -62,6 +63,7 @@ auto register_builtin_losses() {
 	register_loss<T>(factories, "Mape", [](const json& loss) { return new MapeLoss<T>{}; });
 	register_loss<T>(factories, "Smape", [](const json& loss) { return new SmapeLoss<T>{}; });
 	register_loss<T>(factories, "CrossEntropy", [](const json& loss) { return new CrossEntropyLoss<T>{}; });
+	register_loss<T>(factories, "BinaryCrossEntropy", [](const json& loss) { return new BinaryCrossEntropy<T>{}; });
 	register_loss<T>(factories, "Variance", [](const json& loss) { return new VarianceIsLoss<T>{}; });
 
 	return factories;
